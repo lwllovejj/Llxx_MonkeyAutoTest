0f4471ed9cdb251f24e56cc43adcb809b3a7c76d
fanxing <lwl139678@139.com>
2016-09-13 09:10:52 +0800

    feature 测试组和测试单元框架

---
+++ src/llxx_app/llxx_pluggroup.py
@@ -0,0 +1,11 @@
+# --*coding:utf-8*--
+'''
+Created on 2016年9月13日
+
+@author: fanxin, eachen
+@summary: 封装测试组，这里代表了一组测试
+'''
+class PlugGroup:
+    def __init__(self):
+        pass
+    
---
+++ src/llxx_app/llxx_plugunit.py
@@ -0,0 +1,10 @@
+# --*coding:utf-8*--
+'''
+Created on 2016年9月13日
+
+@author: fanxin, eachen
+@summary: 封装测试单元
+'''
+class PlusUnit:
+    def __init__(self):
+        pass
