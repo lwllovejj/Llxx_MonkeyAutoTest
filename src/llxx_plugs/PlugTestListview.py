0eecd43973be1a2c0db2da916d8eefb79c6c1c1e
fanxing <lwl139678@139.com>
2016-09-21 20:50:10 +0800

    feature 测试lisetview 插件

--- src/llxx_plugs/PlugTestListview.py
+++ src/llxx_plugs/PlugTestListview.py
@@ -5,6 +5,11 @@ Created on 2016年9月14日
 @author: fanxin, eachen
 '''
 from llxx_plugunit import PlugUnit
+from llxx_command import UiSelectQuery
+from llxx_node import bounds
+import os
+import time
+
 __all__ = ["PlugTestListview"]
 class PlugTestListview(PlugUnit):
     
@@ -15,3 +20,25 @@ class PlugTestListview(PlugUnit):
     
     def run(self, llxx_client):
         print "test list view"
+        query = UiSelectQuery(llxx_client)
+        listview = query.className("android.widget.ListView").query()
+        if listview != None:
+            print listview
+            if listview['isfind']:
+                listbound = bounds(listview['node']['bounds'])
+                print "adb shell input swipe " + listbound.centerTopToBottom()
+                os.system("adb shell input swipe " + listbound.centerTopToBottom())
+                time.sleep(5)
+                duration = 100
+                count = 0
+                while count < 10:
+                    ## print takeSnapshot.takeSnapshot(currendir + "//snapshot_yule" + str(time.time()) +".png")
+                    cmd = "adb shell input swipe" + " " + listbound.centerBottomToTop() + " " + str(duration)
+                    print cmd
+                    os.system(cmd)
+                    time.sleep(1)
+                    count += 1
+                    #duration += 50
+                print "test list view end"
+        
+        
